19d0b3529fe893621c4921b59fbe57606db57586
JINTO N SEBASTIAN <65418412+jinto8301@users.noreply.github.com>
2022-06-21 15:54:24 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -27,7 +27,7 @@ logger.setLevel(logging.ERROR)
 @Client.on_message(filters.command('id'))
 async def showid(client, message):
     chat_type = message.chat.type
-    if chat_type.name == enums.ChatType.PRIVATE:
+    if chat_type == "private":
         user_id = message.chat.id
         first = message.from_user.first_name
         last = message.from_user.last_name or ""
@@ -38,7 +38,7 @@ async def showid(client, message):
             quote=True
         )
 
-    elif chat_type.name in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
+    elif chat_type in ["group", "supergroup"]:
         _id = ""
         _id += (
             "<b>➲ Chat ID</b>: "
@@ -141,90 +141,6 @@ async def who_is(client, message):
     await status_message.delete()
 
 
-@Client.on_message(filters.command(["imdb", 'search']))
-async def imdb_search(client, message):
-    if ' ' in message.text:
-        k = await message.reply('Searching ImDB')
-        r, title = message.text.split(None, 1)
-        movies = await get_poster(title, bulk=True)
-        if not movies:
-            return await message.reply("No results Found")
-        btn = [
-            [
-                InlineKeyboardButton(
-                    text=f"{movie.get('title')} - {movie.get('year')}",
-                    callback_data=f"imdb#{movie.movieID}",
-                )
-            ]
-            for movie in movies
-        ]
-        await k.edit('Here is what i found on IMDb', reply_markup=InlineKeyboardMarkup(btn))
-    else:
-        await message.reply('Give me a movie / series Name')
-
-
-# @Client.on_callback_query(filters.regex('^imdb'))
-# async def imdb_callback(bot: Client, query: CallbackQuery):
-#     i, movie = query.data.split('#')
-#     imdb = await get_poster(query=movie, id=True)
-#     btn = [
-#         [
-#             InlineKeyboardButton(
-#                 text=f"{imdb.get('title')}",
-#                 url=imdb['url'],
-#             )
-#         ]
-#     ]
-#     if imdb:
-#         caption = IMDB_TEMPLATE.format(
-#             query=imdb['title'],
-#             title=imdb['title'],
-#             votes=imdb['votes'],
-#             aka=imdb["aka"],
-#             seasons=imdb["seasons"],
-#             box_office=imdb['box_office'],
-#             localized_title=imdb['localized_title'],
-#             kind=imdb['kind'],
-#             imdb_id=imdb["imdb_id"],
-#             cast=imdb["cast"],
-#             runtime=imdb["runtime"],
-#             countries=imdb["countries"],
-#             certificates=imdb["certificates"],
-#             languages=imdb["languages"],
-#             director=imdb["director"],
-#             writer=imdb["writer"],
-#             producer=imdb["producer"],
-#             composer=imdb["composer"],
-#             cinematographer=imdb["cinematographer"],
-#             music_team=imdb["music_team"],
-#             distributors=imdb["distributors"],
-#             release_date=imdb['release_date'],
-#             year=imdb['year'],
-#             genres=imdb['genres'],
-#             poster=imdb['poster'],
-#             plot=imdb['plot'],
-#             rating=imdb['rating'],
-#             url=imdb['url'],
-#             **locals()
-#         )
-#     else:
-#         caption = "No Results"
-#     if imdb.get('poster'):
-#         try:
-#             await query.message.reply_photo(photo=imdb['poster'], caption=caption,
-#                                             reply_markup=InlineKeyboardMarkup(btn))
-#         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
-#             pic = imdb.get('poster')
-#             poster = pic.replace('.jpg', "._V1_UX360.jpg")
-#             await query.message.reply_photo(photo=imdb['poster'], caption=caption,
-#                                             reply_markup=InlineKeyboardMarkup(btn))
-#         except Exception as e:
-#             logger.exception(e)
-#             await query.message.reply(caption, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=False)
-#         await query.message.delete()
-#     else:
-#         await query.message.edit(caption, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=False)
-#     await query.answer()
 
 
 @Client.on_message(filters.command(["speech"]))
@@ -333,24 +249,6 @@ async def paste_func(_, message):
     await m.delete()
 
 
-@Client.on_message(filters.regex(r'https?://[^\s]+') & filters.private)
-async def link_handler(bot, message):
-    link = message.matches[0].group(0)
-    try:
-        short_link = await get_shortlink(link)
-        await message.reply(f'Here Is Your [Short Link] 👉🏻 {short_link}', quote=True)
-    except Exception as e:
-        await message.reply(f'Error: {e}', quote=True)
-
-
-async def get_shortlink(link):
-    url = 'https://gplinks.in/api'
-    params = {'api': SHORT_LINK_API_KEY, 'url': link}
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params, raise_for_status=True) as response:
-            data = await response.json()
-            return data["shortenedUrl"]
 
 
 def convert(text):
