579e8e266726ace17a4c62c518eeb3ae3000e01d
JINTO N SEBASTIAN <65418412+jinto8301@users.noreply.github.com>
2022-06-21 20:06:19 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -56,13 +56,13 @@ async def start(client, message):
         await message.delete()
             
             # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
-            if not await db.get_chat(message.chat.id):
+        if not await db.get_chat(message.chat.id):
                 total = await client.get_chat_members_count(message.chat.id)
                 await client.send_message(LOG_CHANNEL,
                                           script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total,
                                                                    "Unknown"))
                 await db.add_chat(message.chat.id, message.chat.title)
-            return
+        return
         else:
             btn = [[
                 InlineKeyboardButton("⭕️ᴘᴍ ᴍᴇ ⭕️", url="https://t.me/jintons")
